c374f02f30780b7fba099954e21321483e48ee15
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-15 16:27:13 -0300

    read feito

--- CRUD_DRF/API/models.py
+++ CRUD_DRF/API/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    username = models.CharField(max_length = 255)
+    email = models.EmailField()
+    senha = models.CharField(max_length = 50)
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.username
--- CRUD_DRF/CRUD_DRF/urls.py
+++ CRUD_DRF/CRUD_DRF/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from API import viewsets
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'USUARIOS/',viewsets.UsuarioViewSet,basename = 'usuario')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
 ]
