20d154795bacdfceaa7a5adfc134734405e1001d
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-15 19:43:14 -0300

    cadastro e visualização de tarefas prontos

--- TO-DO-LIST/ListApp/models.py
+++ TO-DO-LIST/ListApp/models.py
@@ -6,8 +6,10 @@ class Tarefa(models.Model):
     titulo = models.CharField(max_length = 55)
     descricao = models.TextField()
     data_criacao = models.DateField(auto_now_add = True)
-    atualizacao = models.DateField(auto_now_add = True)
-    concluida = models.BooleanField(default = 1)
+    data_tarefa = models.DateField(auto_now_add = False)
+    data_concluido = models.DateField(null=True)
+    atualizacao = models.DateField(null = True)
+    concluida = models.BooleanField(default = False)
 
     def __str__(self):
         return self.titulo
--- TO-DO-LIST/ListApp/views.py
+++ TO-DO-LIST/ListApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import Tarefa
 # Create your views here.
+
+# visualizar a tarefa
+def view_tarefa(request):
+    tarefa = Tarefa.objects.all()
+    return render(request,'viewtarefa.html',{'contexto':tarefa})
+
+
+
+# tela home
+
+def home(request):
+    if request.method == 'POST':
+        tarefa = Tarefa()
+        tarefa.titulo = request.POST.get('titulo')
+        tarefa.descricao = request.POST.get('descricao')
+        tarefa.data_tarefa = request.POST.get('data_tarefa')
+        tarefa.save()
+    return render(request,'home.html')
